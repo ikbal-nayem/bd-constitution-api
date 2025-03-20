66c2b3ac3552645d81a20e638798f7c6c5c9182d
Ikbal <ikbalnayem000@gmail.com>
2025-03-20 14:41:22 +0600

    receive message

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from retrival import getAnswer
 
-# Create a FastAPI application
+
+class Message(BaseModel):
+    role: str
+    content: str
+    id: str = None
+
+
+class ChatRequest(BaseModel):
+    messages: list[Message]
+    max_tokens: int = 100
+    temperature: float = 0.5
+
+
 app = FastAPI()
 
 
@@ -11,7 +24,7 @@ def read_root():
     return {"message": "Hello, FastAPI!"}
 
 
-@app.get("/chat")
-def chat_response():
-    res = getAnswer("What is the capital of Bangladesh?")
+@app.post("/chat")
+def chat_response(request: ChatRequest):
+    res = getAnswer(request.messages[-1].content)
     return {"message": res}
